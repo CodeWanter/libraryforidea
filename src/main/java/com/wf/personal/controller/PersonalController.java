175564272a4d82701c741368a231970d4aa633e7
huangjunqing <1767943686@qq.com>
2018-08-07 16:17:04 +0800

    学习

--- src/main/java/com/wf/personal/controller/PersonalController.java
+++ src/main/java/com/wf/personal/controller/PersonalController.java
@@ -3,18 +3,9 @@
  */
 package com.wf.personal.controller;
 
-import com.wf.commons.result.PageInfo;
-import com.wf.commons.utils.StringUtils;
-import com.wf.model.Article;
-import com.wf.personal.service.IArticleService;
 import org.apache.shiro.SecurityUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author zhanghuaiyu
