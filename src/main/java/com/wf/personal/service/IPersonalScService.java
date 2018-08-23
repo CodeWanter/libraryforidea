e5422147e9039357958c47afa8f6e3234a153ecd
huangjunqing <1767943686@qq.com>
2018-08-23 16:39:17 +0800

    判断是否收藏

--- src/main/java/com/wf/personal/service/IPersonalScService.java
+++ src/main/java/com/wf/personal/service/IPersonalScService.java
@@ -1,8 +1,11 @@
 package com.wf.personal.service;
 
+import java.util.List;
+
 import com.baomidou.mybatisplus.service.IService;
 import com.wf.model.PersonalSc;
 
 public interface IPersonalScService extends IService<PersonalSc>{
 	void insertByPsc(PersonalSc psc);
+	List<PersonalSc> selectByUIdAndEId(long userId, long eId);
 }
--- src/main/java/com/wf/personal/service/impl/PersonalScServiceImpl.java
+++ src/main/java/com/wf/personal/service/impl/PersonalScServiceImpl.java
@@ -1,8 +1,14 @@
 package com.wf.personal.service.impl;
+import java.util.List;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.wf.model.PersonalSc;
+import com.wf.model.User;
+import com.wf.model.vo.UserVo;
 import com.wf.personal.mapper.PersonalScMapper;
 import com.wf.personal.service.IPersonalScService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +21,14 @@ public class PersonalScServiceImpl extends ServiceImpl<PersonalScMapper, Persona
 	@Override
 	public void insertByPsc(PersonalSc psc) {
 		this.insert(psc);
+	}
+
+	@Override
+	public List<PersonalSc> selectByUIdAndEId(long userId, long eId) {
+		PersonalSc personalSc = new PersonalSc();
+		personalSc.setUserId(userId);
+		personalSc.setEssayId(eId);
+		EntityWrapper<PersonalSc> wrapper = new EntityWrapper<PersonalSc>(personalSc);
+		return this.selectList(wrapper);
 	}
 }
